772034239ec039fbc2c16108c21a022afca4ea25
to.to081244 <kohei@tsujiharaakiratairanoMacBook-Air.local>
2023-07-21 13:31:48 +0900

    2回目　急行カード作成中

---
+++ src/main/java/card/Kyuko.java
@@ -0,0 +1,11 @@
+package card;
+
+import java.util.ArrayList;
+
+public abstract class Kyuko {
+
+//    public abstract void use(){
+//        ArrayList<Dice> dice2 = new ArrayList<Dice>();
+//    サイコロがインスタンス化されていないと使えないのでエラーになる
+
+}
