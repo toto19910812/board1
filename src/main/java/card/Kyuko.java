9b79f46ab18274005e54166726408279a62875a5
to.to081244 <kohei@tsujiharaakiratairanoMacBook-Air.local>
2023-07-22 12:26:18 +0900

    3回目　急行カード追記中。次は進むコマンドと、マップ作成。

--- src/main/java/card/Kyuko.java
+++ src/main/java/card/Kyuko.java
@@ -1,11 +1,19 @@
 package card;
-
 import java.util.ArrayList;
-
 public abstract class Kyuko {
+    private int number;//サイコロの個数
 
-//    public abstract void use(){
-//        ArrayList<Dice> dice2 = new ArrayList<Dice>();
-//    サイコロがインスタンス化されていないと使えないのでエラーになる
+    public Kyuko() {
+        this.number = 2;
+    }
+    public void use(){
+        ArrayList<Dice> dices = new ArrayList<Dice>();
+        for(int i = 0; i < number; i++){
+            Dice d = new Dice();
+            dices.add(d);
+            //△サイコロを作った。次は目の判定と、合算した値を戻り値にする。
+        }
+
+    }
 
 }
